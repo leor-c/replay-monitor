393464e7b87e810e7ba690d2cf366df46d8e4a21
liorcohen5 <liorcohen5@gmail.com>
2021-01-10 14:56:08 +0200

    implemented log selection, trajectory selection, image state plotting, and fixed bugs

--- visualizer.py
+++ visualizer.py
@@ -1,11 +1,11 @@
 import argparse
 from enum import Enum, auto
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
 from bokeh.io import curdoc
 # from bokeh.io import show
-from bokeh.models import Panel, Tabs, Slider, ColumnDataSource
+from bokeh.models import Panel, Tabs, Slider, ColumnDataSource, Select, Spinner
 from bokeh.plotting import figure
 from bokeh.layouts import column, row
 
@@ -39,6 +39,8 @@ def get_transition(log_id: str, trajectory_index: int, transition_index: int):
 def create_state_data_dict_from_state(state: Tuple[np.ndarray]):
     state_data_dicts = []
     for element in state:
+        if element.shape[0] == 1:
+            element = element[0]
         data = {}
         plot_type = determine_state_element_plot_type(element)
         if plot_type == StateElementPlotType.BAR:
@@ -49,29 +51,49 @@ def create_state_data_dict_from_state(state: Tuple[np.ndarray]):
         elif plot_type == StateElementPlotType.MATRIX:
             data['state_element'] = [element]
             # data['state_x_range_2d'] = element.shape[1]
+        elif plot_type == StateElementPlotType.COLOR_IMAGE:
+            assert element.shape[2] == 3  # Assume color channel is last!
+            xdim, ydim = element.shape[0:2]
+            img = np.empty((xdim, ydim), dtype=np.uint32)
+            view = img.view(dtype=np.uint8).reshape((xdim, ydim, 4))
+            view[:, :, :-1] = np.flipud(element)
+            view[:, :, -1] = 255
+            data['state_element'] = [img]
         state_data_dicts.append(data)
     return state_data_dicts
 
 
 def step_slider_change_handler(attr, old, new):
-    trajectory_index = 0
-    s, a, r, s2 = get_transition(log_ids[0], trajectory_index, new)
-    # print(f'state={s}, shape={s[0].shape}')
-    state_data_dicts = create_state_data_dict_from_state(s)
-    next_state_data_dicts = create_state_data_dict_from_state(s2)
-    for i, element in enumerate(s):
-        state_data_sources[i].stream(new_data=state_data_dicts[i], rollover=state_elements_sizes[i])
-        next_state_data_sources[i].stream(new_data=next_state_data_dicts[i], rollover=state_elements_sizes[i])
+    global data_manager, ui_manager
+    data_manager.change_transition(new)
 
-    print(f'old value={old}, new value={new}. state={s}')
+    ui_manager.update_ui_due_to_transition_change()
+
+
+def trajectory_changed_handler(attr, old, new):
+    global data_manager, ui_manager
+    data_manager.change_transition(0, new)
+
+    ui_manager.update_ui_due_to_trajectory_change()
+
+
+def log_select_change_handler(attr, old, new):
+    # TODO: implement (call rebuild function of the states layouts)
+    global data_manager, ui_manager
+    data_manager.change_transition(0, 0, new)
+    ui_manager.update_ui_due_to_log_change()
 
 
 class StateElementPlotType(Enum):
     BAR = auto()
     MATRIX = auto()
+    COLOR_IMAGE = auto()
 
 
 def determine_state_element_plot_type(state_elem: np.ndarray) -> StateElementPlotType:
+    if len(state_elem.shape) > 1 and state_elem.shape[0] == 1:
+        state_elem = state_elem[0]
+
     if len(state_elem.shape) == 1:
         return StateElementPlotType.BAR
     elif len(state_elem.shape) == 2:
@@ -79,10 +101,15 @@ def determine_state_element_plot_type(state_elem: np.ndarray) -> StateElementPlo
             return StateElementPlotType.BAR
         else:
             return StateElementPlotType.MATRIX
+    elif len(state_elem.shape) == 3 and state_elem.shape[2] == 3:
+        return StateElementPlotType.COLOR_IMAGE
 
 def create_state_layout(state, data_sources):
     tabs_list = []
     for i, element in enumerate(state):
+        if len(element.shape) > 1 and element.shape[0] == 1:
+            element = element[0]
+
         plot_type = determine_state_element_plot_type(element)
         if plot_type == StateElementPlotType.BAR:
             fig = figure(plot_width=600, plot_height=600)
@@ -92,36 +119,123 @@ def create_state_layout(state, data_sources):
             fig = figure(plot_width=600, plot_height=600, x_range=(0, range_max), y_range=(0, range_max))
             fig.image(image='state_element', x=0, y=0, dw=element.shape[1], dh=element.shape[0], palette="Spectral11",
                       source=data_sources[i])
+        elif plot_type == StateElementPlotType.COLOR_IMAGE:
+            range_max = max(element.shape[1], element.shape[0])
+            fig = figure(plot_width=600, plot_height=600, x_range=(0, range_max), y_range=(0, range_max))
+            fig.image_rgba(image='state_element', x=0, y=0, dw=element.shape[1], dh=element.shape[0],
+                           source=data_sources[i])
         else:
             continue
 
         tabs_list.append(Panel(child=fig, title=f"State Element {i}"))
     return Tabs(tabs=tabs_list)
 
-log_ids, trajectories_lengths, n_state_elements = read_metadata()
 
-s, a, r, s2 = get_transition(log_ids[0], 0, 0)
+class DataManager:
+    def __init__(self):
+        db_reader = DBReader(**kwargs)
+        self.log_ids = db_reader.get_logs_ids()
+        self.current_log = self.log_ids[0]
+        self.trajectory_index = 0
+        self.transition_index = 0
+        self.trajectories_lengths = db_reader.get_trajectories_lengths(self.current_log)
+        self.n_state_elements = db_reader.get_num_of_state_elements(self.current_log)
 
-state_elements_sizes = [state_elem.size for state_elem in s]
+        self.s, self.a, self.r, self.s2 = get_transition(self.current_log, self.trajectory_index, self.transition_index)
 
-state_data_sources = [ColumnDataSource(data=data_dict) for data_dict in create_state_data_dict_from_state(s)]
-next_state_data_sources = [ColumnDataSource(data=data_dict) for data_dict in create_state_data_dict_from_state(s2)]
+        self.state_elements_sizes = [state_elem.size for state_elem in self.s]
 
-slider = Slider(start=0, end=trajectories_lengths[0], value=0, step=1, title="Time Step")
-slider.on_change('value', step_slider_change_handler)
+    def change_transition(self, transition_index: int, trajectory_index: int = None, log_id: str = None):
+        if trajectory_index is not None:
+            self.trajectory_index = trajectory_index
+        self.transition_index = transition_index
+        is_log_changed = False
+        if log_id is not None:
+            is_log_changed = log_id != self.current_log
+            self.current_log = log_id
+        self.s, self.a, self.r, self.s2 = get_transition(self.current_log, self.trajectory_index, self.transition_index)
+        if log_id is not None and is_log_changed:
+            db_reader = DBReader(**kwargs)
+            self.trajectories_lengths = db_reader.get_trajectories_lengths(self.current_log)
+            self.n_state_elements = db_reader.get_num_of_state_elements(self.current_log)
+            self.state_elements_sizes = [state_elem.size for state_elem in self.s]
 
-# p1 = figure(plot_width=300, plot_height=300)
-# p1.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
-# tab1 = Panel(child=p1, title="circle")
-#
-# p2 = figure(plot_width=300, plot_height=300)
-# p2.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=3, color="navy", alpha=0.5)
-# tab2 = Panel(child=p2, title="line")
-##Tabs(tabs=[tab1, tab2])
+    def get_current_trajectory_length(self):
+        return self.trajectories_lengths[self.trajectory_index]
 
-state_layout = create_state_layout(s, state_data_sources)
-next_state_layout = create_state_layout(s2, next_state_data_sources)
 
-layout = column(row(state_layout, next_state_layout), slider)
+class UIManager:
+    def __init__(self, data_manager: DataManager):
+        self.data_manager = data_manager
 
-curdoc().add_root(layout)
+        self.log_select = Select(title="Choose a log:", value=data_manager.current_log, options=data_manager.log_ids)
+        self.log_select.on_change('value', log_select_change_handler)
+
+        self.step_slider = None
+        self._generate_step_slider()
+
+        self.trajectory_select = None
+        self._generate_trajectory_select()
+
+        self.state_data_sources = None
+        self.next_state_data_sources = None
+        self.state_layout = None
+        self.next_state_layout = None
+        self.states_layout = None
+        self._create_states_ui()
+
+        self.states_layout = row(self.state_layout, self.next_state_layout)
+        self.sliders_layout = column(self.step_slider, self.trajectory_select, sizing_mode='stretch_width')
+        self.layout = column(self.log_select, self.states_layout, self.sliders_layout)
+
+    def update_ui_due_to_log_change(self):
+        self._create_states_ui()
+        self.states_layout.children = [self.state_layout, self.next_state_layout]
+
+        self.step_slider.value = self.data_manager.transition_index
+        self.step_slider.end = self.data_manager.get_current_trajectory_length()-1
+
+    def update_ui_due_to_transition_change(self):
+        state_data_dicts = create_state_data_dict_from_state(self.data_manager.s)
+        next_state_data_dicts = create_state_data_dict_from_state(self.data_manager.s2)
+        for i, element in enumerate(self.data_manager.s):
+            self.state_data_sources[i].stream(new_data=state_data_dicts[i],
+                                                    rollover=self.data_manager.state_elements_sizes[i])
+            self.next_state_data_sources[i].stream(new_data=next_state_data_dicts[i],
+                                                         rollover=self.data_manager.state_elements_sizes[i])
+
+    def update_ui_due_to_trajectory_change(self):
+        self.update_ui_due_to_transition_change()
+
+        self.step_slider.value = self.data_manager.transition_index
+        self.step_slider.end = self.data_manager.get_current_trajectory_length() - 1
+
+    def _generate_step_slider(self):
+        print(f'slider end: {self.data_manager.get_current_trajectory_length()}')
+        self.step_slider = Slider(start=0, end=self.data_manager.get_current_trajectory_length()-1,
+                                  value=self.data_manager.transition_index, step=1, title="Time Step")
+        self.step_slider.on_change('value', step_slider_change_handler)
+
+    def _generate_trajectory_select(self):
+        self.trajectory_select = Spinner(
+            title="Choose a trajectory:",
+            low=0,
+            high=len(self.data_manager.trajectories_lengths)-1,
+            step=1,
+            value=self.data_manager.trajectory_index,
+        )
+        self.trajectory_select.on_change('value', trajectory_changed_handler)
+
+    def _create_states_ui(self):
+        self.state_data_sources = [ColumnDataSource(data=data_dict)
+                                   for data_dict in create_state_data_dict_from_state(self.data_manager.s)]
+        self.next_state_data_sources = [ColumnDataSource(data=data_dict)
+                                        for data_dict in create_state_data_dict_from_state(self.data_manager.s2)]
+        self.state_layout = create_state_layout(self.data_manager.s, self.state_data_sources)
+        self.next_state_layout = create_state_layout(self.data_manager.s2, self.next_state_data_sources)
+
+data_manager = DataManager()
+
+ui_manager = UIManager(data_manager=data_manager)
+
+curdoc().add_root(ui_manager.layout)
