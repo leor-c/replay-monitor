611ce61b5979549e6f69ba0e199eed15a15fc9d7
liorcohen5 <liorcohen5@gmail.com>
2021-01-18 12:49:30 +0200

    packaging + improvements

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="replay-monitor", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Leor Cohen",
     author_email="liorcohen5@gmail.com",
     description="A tool for easy data exploration in reinforcement learning environments.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/liorcohen5/replay-monitor",
+    download_url="https://github.com/liorcohen5/replay-monitor/archive/0.0.1.tar.gz",
     keywords=['reinforcement learning', 'tool', 'data exploration', 'replay', 'monitor', 'analytical tool'],
     packages=setuptools.find_packages(),
     classifiers=[
@@ -19,6 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    Install_requires=['gym', 'bokeh', 'tensorflow', 'numpy'],
     entry_points={
         'console_scripts': ['replay-monitor=replay_monitor.visualizer:start_server'],
     },
